7b5c2da62b92a3fdb90f7a465b9e01543fac15d3
jaydaleon05 <jaydaleon@gmail.com>
2024-06-20 10:48:55 +0800

    uploaded the groom and boarding table

--- app/DB/boarding_tables.sql
+++ app/DB/boarding_tables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2024 at 12:02 PM
+-- Generation Time: Jun 20, 2024 at 04:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,7 +43,24 @@ CREATE TABLE `boarding_tables` (
 --
 
 INSERT INTO `boarding_tables` (`board_id`, `board_vet_id`, `vet_name`, `board_inclusions`, `board_price`, `board_image`, `created_at`, `updated_at`) VALUES
-(1, 4, 'pawpatrol', 'WALANG FOOD', 50, '1718704076.webp', '2024-06-18 01:47:56', '2024-06-18 01:59:12');
+(1, 4, 'pawpatrol', 'WALANG FOOD', 50, '1718704076.webp', '2024-06-18 01:47:56', '2024-06-18 01:59:12'),
+(2, 8, 'Petformance Veterinary- Animal Health and Wellness Clinic', 'None', 400, '1718720137.jpg', '2024-06-18 06:15:37', '2024-06-18 06:15:37'),
+(3, 11, 'EMC Veterinary Clinic', 'None', 400, '1718720625.jpg', '2024-06-18 06:23:45', '2024-06-18 06:23:45'),
+(4, 17, 'Blue Pet', 'Food', 350, '1718723122.jpg', '2024-06-18 07:05:22', '2024-06-18 07:05:22'),
+(5, 18, 'Oasis Animal Clinic', 'Food', 400, '1718723499.jpg', '2024-06-18 07:11:39', '2024-06-18 07:11:39'),
+(6, 19, 'Cresent Paws', 'Food', 400, '1718723834.jpg', '2024-06-18 07:17:14', '2024-06-18 07:17:14'),
+(7, 20, 'Golden Griffin Animal Clinic', 'Food', 400, '1718724058.jpg', '2024-06-18 07:20:58', '2024-06-18 07:20:58'),
+(8, 26, 'AAV Animal Care Clinic', 'None', 300, '1718761054.jpg', '2024-06-18 17:37:34', '2024-06-18 17:37:34'),
+(9, 27, 'Serbisyo Beterinaryo Animal Hospital', 'Food', 400, '1718762455.jpg', '2024-06-18 18:00:55', '2024-06-18 18:00:55'),
+(10, 29, 'Petfriends Veterinary Clinic and Grooming Center', 'Food', 450, '1718763291.jpg', '2024-06-18 18:14:51', '2024-06-18 18:14:51'),
+(11, 30, 'Furry Friend Veterinary Clinic', 'Food', 450, '1718763539.jpg', '2024-06-18 18:18:59', '2024-06-18 18:18:59'),
+(12, 31, 'VetLink Animal Clinic', 'Food', 500, '1718763825.jpg', '2024-06-18 18:23:45', '2024-06-18 18:23:45'),
+(13, 33, 'Happy Paws Pet Shop', 'Food', 500, '1718773735.jpg', '2024-06-18 21:08:55', '2024-06-18 21:08:55'),
+(14, 34, 'Pawsetivety Veterinary Clinic', 'Food', 550, '1718774007.jpg', '2024-06-18 21:13:27', '2024-06-18 21:13:27'),
+(15, 35, 'Garcia Veterinary and Grooming Center', 'None', 300, '1718774191.jpg', '2024-06-18 21:16:31', '2024-06-18 21:16:31'),
+(16, 36, 'Happy Paws Pet Shop', 'Food', 500, '1718777026.jpg', '2024-06-18 22:03:46', '2024-06-18 22:03:46'),
+(17, 37, 'Pawsetivety Veterinary Clinic', 'Food', 550, '1718777192.jpg', '2024-06-18 22:06:32', '2024-06-18 22:06:32'),
+(18, 38, 'Garcia Veterinary and Grooming Center', 'None', 300, '1718777455.jpg', '2024-06-18 22:10:55', '2024-06-18 22:10:55');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +80,7 @@ ALTER TABLE `boarding_tables`
 -- AUTO_INCREMENT for table `boarding_tables`
 --
 ALTER TABLE `boarding_tables`
-  MODIFY `board_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `board_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- app/DB/groom_tables.sql
+++ app/DB/groom_tables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 18, 2024 at 12:01 PM
+-- Generation Time: Jun 20, 2024 at 04:46 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,7 +43,24 @@ CREATE TABLE `groom_tables` (
 --
 
 INSERT INTO `groom_tables` (`groom_id`, `groom_vet_id`, `vet_name`, `groom_details`, `groom_price`, `groom_image`, `created_at`, `updated_at`) VALUES
-(2, 4, 'pawpatrol', 'Full Groom', 400, '1718702710.webp', '2024-06-18 01:25:10', '2024-06-18 01:25:10');
+(2, 4, 'pawpatrol', 'Full Groom', 400, '1718702710.webp', '2024-06-18 01:25:10', '2024-06-18 01:25:10'),
+(3, 8, 'Petformance Veterinary- Animal Health and Wellness Clinic', 'Full Groom Inclusion', 500, '1718720079.jpg', '2024-06-18 06:14:39', '2024-06-18 06:14:39'),
+(4, 11, 'EMC Veterinary Clinic', 'Full Groom Inclusion', 500, '1718720578.jpg', '2024-06-18 06:22:58', '2024-06-18 06:22:58'),
+(5, 17, 'Blue Pet', 'Full Groom Inclusion', 500, '1718723067.jpg', '2024-06-18 07:04:27', '2024-06-18 07:04:27'),
+(6, 18, 'Oasis Animal Clinic', 'Full Groom Inclusion', 480, '1718723330.jpg', '2024-06-18 07:08:50', '2024-06-18 07:08:50'),
+(7, 19, 'Cresent Paws', 'Haircut', 400, '1718723781.jpg', '2024-06-18 07:16:21', '2024-06-18 07:16:21'),
+(8, 20, 'Golden Griffin Animal Clinic', 'Haircut', 400, '1718724007.jpg', '2024-06-18 07:20:07', '2024-06-18 07:20:07'),
+(9, 26, 'AAV Animal Care Clinic', 'Haircut', 500, '1718761006.png', '2024-06-18 17:36:46', '2024-06-18 17:36:46'),
+(10, 27, 'Serbisyo Beterinaryo Animal Hospital', 'Full Groom Inclusion', 500, '1718761547.jpg', '2024-06-18 17:45:47', '2024-06-18 17:45:47'),
+(11, 29, 'Petfriends Veterinary Clinic and Grooming Center', 'Haircut', 450, '1718763246.jpg', '2024-06-18 18:14:06', '2024-06-18 18:14:06'),
+(12, 30, 'Furry Friend Veterinary Clinic', 'Full Groom Inclusion', 600, '1718763508.jpg', '2024-06-18 18:18:28', '2024-06-18 18:18:28'),
+(13, 31, 'VetLink Animal Clinic', 'Haircut', 500, '1718763776.jpg', '2024-06-18 18:22:56', '2024-06-18 18:22:56'),
+(14, 33, 'Happy Paws Pet Shop', 'Full Groom Inclusion', 500, '1718773686.jpg', '2024-06-18 21:08:06', '2024-06-18 21:08:06'),
+(15, 34, 'Pawsetivety Veterinary Clinic', 'Full Groom Inclusion', 600, '1718773986.jpg', '2024-06-18 21:13:06', '2024-06-18 21:13:06'),
+(16, 35, 'Garcia Veterinary and Grooming Center', 'Haircut', 500, '1718774162.jpg', '2024-06-18 21:16:02', '2024-06-18 21:16:02'),
+(17, 36, 'Happy Paws Pet Shop', 'Full Groom Inclusion', 500, '1718776992.jpg', '2024-06-18 22:03:12', '2024-06-18 22:03:12'),
+(18, 37, 'Pawsetivety Veterinary Clinic', 'Full Groom Inclusion', 600, '1718777174.jpg', '2024-06-18 22:06:14', '2024-06-18 22:06:14'),
+(19, 38, 'Garcia Veterinary and Grooming Center', 'Haircut', 500, '1718777416.jpg', '2024-06-18 22:10:16', '2024-06-18 22:10:16');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +80,7 @@ ALTER TABLE `groom_tables`
 -- AUTO_INCREMENT for table `groom_tables`
 --
 ALTER TABLE `groom_tables`
-  MODIFY `groom_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `groom_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
