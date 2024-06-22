fa6cf4ce271b64c594c553842e6f1d2f46090884
jaydaleon05 <jaydaleon@gmail.com>
2024-06-22 17:34:08 +0800

    edited text area

--- resources/views/user-profile.blade.php
+++ resources/views/user-profile.blade.php
@@ -13,16 +13,14 @@
 
         <div class="user-profile row m-5 d-flex borange">
             <div class="container mt-3  ">
-                <img src="/img/user-icon.png" alt="" class="bblack">
+                <img src="/img/user-icon.png" alt="" class="bblack rounded-circle">
                 <h3 class="mt-3">{{Session::get('fname')}} {{Session::get('lname')}}</h3>
                 <p>Settings</p>
                 <div class="row bteal text-center mb-3">
                     <form action="/user-profile" method="post">
                         @csrf
                         <h2 class="mt-3">Write a Review</h2>
-                        <textarea class="mt-3" name="review" id="" cols="150" rows="10" placeholder="Write a review..."required>
-    
-                        </textarea>
+                        <textarea class="mt-3" name="review" id="" cols="150" rows="10" placeholder="Write a review..."required></textarea>
                         <span class="input-group-text" id="basic-addon1">Rating</span>
                         <select name="rating" class="form-control" id="">
                             <option value="1" >1 Star</option>
